bff035dbc453af4c747a97dd5f1b5376cff2f3af
Majid chouaib <majidchouaib5@gmail.com>
2021-10-10 21:55:35 +0200

    firstCommit

---
+++ src/features/counterSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchCount } from './counterAPI';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+export const incrementAsync = createAsyncThunk(
+  'counter/fetchCount',
+  async (amount) => {
+    const response = await fetchCount(amount);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+  }
+});
+
+export const { increment, decrement, incrementByAmount } = apprSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectCount = (state) => state.counter.value;
+
+// We can also write thunks by hand, which may contain both sync and async logic.
+// Here's an example of conditionally dispatching actions based on current state.
+
+export default appSlice.reducer;
