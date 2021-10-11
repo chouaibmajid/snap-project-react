35b35a36a606e3a25cfce2acbf12df0504527953
Majid chouaib <majidchouaib5@gmail.com>
2021-10-11 21:31:43 +0200

    2 commit

--- src/Preview.js
+++ src/Preview.js
@@ -3,14 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import CloseIcon from "@mui/icons-material/Close";
 import "./Preview.css";
-import TextFieldsIcon from '@mui/icons-material/TextFields';
-import CreateIcon from '@mui/icons-material/Create';
-import NoteIcon from '@mui/icons-material/Note';
-import MusicNoteIcon from '@mui/icons-material/MusicNote'
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import CropIcon from '@mui/icons-material/Crop';
-import TimerIcon from '@mui/icons-material/Timer';
-import SendIcon from '@mui/icons-material/Send';
+import TextFieldsIcon from "@mui/icons-material/TextFields";
+import CreateIcon from "@mui/icons-material/Create";
+import NoteIcon from "@mui/icons-material/Note";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import CropIcon from "@mui/icons-material/Crop";
+import TimerIcon from "@mui/icons-material/Timer";
+import SendIcon from "@mui/icons-material/Send";
+import { db, storage } from "./firebase";
+import { v4 as id } from "uuid";
 function Preview() {
   const history = useHistory();
   const dispatch = useDispatch();
@@ -27,20 +29,24 @@ function Preview() {
       type: "resetImage",
     });
   };
+  const sendImage = () => {
+    const id = uuid();
+    const uploadImg = storage.ref(`posts/${id}`).putString(images, "imgUrl");
+  };
   return (
     <div className="P">
-      <CloseIcon  onClick={closeClicked} className="P__close" />
-<div className="P__toolbarRight">
-<TextFieldsIcon />
-<CreateIcon />
-<NoteIcon />
-<MusicNoteIcon />
-<AttachFileIcon />
-<TimerIcon />
-</div>
+      <CloseIcon onClick={closeClicked} className="P__close" />
+      <div className="P__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <TimerIcon />
+      </div>
       <img src={images} alt="" />
-      <div className="P__buttomSend" >
-          Envoyez à <SendIcon className="P__send"/>
+      <div onClick={sendImage} className="P__buttomSend">
+        Envoyez à <SendIcon className="P__send" />
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,14 @@ const firebaseConfig = {
   projectId: "snap-chat-clone-projet",
   storageBucket: "snap-chat-clone-projet.appspot.com",
   messagingSenderId: "108164056870",
-  appId: "1:108164056870:web:2651cd3435ff40b79f63e7"
+  appId: "1:108164056870:web:2651cd3435ff40b79f63e7",
 };
 
 // Initialize Firebase
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore;
-const auth = firebase.auth();
-const storage = firebase.storage();
-const provider = new firebase.auth.GoogleAuthProvider();
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const db = firebaseApp.firestore;
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const provider = new firebase.auth.GoogleAuthProvider();
 
-export {db, auth , storage , provider}
+
